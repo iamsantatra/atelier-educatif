45afeb29765d4111fec01b35d56d8694671c3423
marie Fiombonana <marie.ahthan@gmail.com>
2022-05-21 10:38:57 +0300

    front recherche animal

--- AtelierEducatif/app/src/main/java/com/example/ateliereducatif/service/EntiteService.java
+++ AtelierEducatif/app/src/main/java/com/example/ateliereducatif/service/EntiteService.java
@@ -1,13 +1,23 @@
 package com.example.ateliereducatif.service;
 
+import com.example.ateliereducatif.model.Entite;
 import com.example.ateliereducatif.model.reponse.EntiteRep;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface EntiteService {
   static String BASE_URL = "entite/";
 
   @GET(BASE_URL+"liste_animaux")
   Call<EntiteRep> liste_animaux();
+
+//  @GET(BASE_URL+"recherche_animal")
+//  Call<EntiteRep> (@Path("nom") String nom,
+//                             @Query("api_key") String apiKey);
+
 }
